f1924a844be7809aabb2748f4d91a1063b97c10d
bob <ehdrud1332@gmail.com>
2020-03-07 11:06:23 +0900

    update userModel

--- model/user.js
+++ model/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const gravatar = required('gravatar');
+const bcrypt = required('bcryptjs');
+
 
 const userSchema = mongoose.Schema(
     {
@@ -24,7 +27,30 @@ const userSchema = mongoose.Schema(
     }
 );
 // 비동기 방식의 함수 실행
-// 
-userSchema.pre("save", async function)
+// async : 사용자 입력고 동시에 비동기 실챙
+userSchema.pre("save", async function (next) {
+    try {
+        console.log('enter');
+        // 아바타의 url 생성
+        // this 파일안에 상수를 불러오기 위해 사용함
+        const avatar = await gravatar.url(this.email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+        this.avatar = avatar;
+
+
+        //password 암호화
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(this.password, salt);
+        this.password = passwordHash;
+        console.log('exited');
+        next();
+
+    } catch(error) {
+        next(error);
+    }
+});
 
 module.exports = mongoose.model("user", userSchema);
