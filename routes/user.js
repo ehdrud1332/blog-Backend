985c20d0428a4b2e16c336ecc7b54e14ebc773a2
bob <ehdrud1332@gmail.com>
2020-03-07 10:40:10 +0900

    add API 설계 주석

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 //회원가입
+// @route POST http://localhost:2055/user/signup
+// @desc user signup
+// @access Public
 router.post('/signup', (req, res) => {
     res.json({
         msg: "회원가입 성공"
@@ -9,12 +12,18 @@ router.post('/signup', (req, res) => {
 });
 
 //로그인
+// @route POST http://localhost:2055/user/login
+// @desc user login(return jsonwebtoken 토큰 발행 검사)
+// @access Public
 router.post('/login', (req, res) => {
     res.json({
         msg: "로그인 성공"
     })
 })
 //회원 정보
+// @route GET http://localhost:2055/user
+// @desc user get all
+// @access Private
 router.get('/', (req, res) => {
     res.json({
         msg: "정보 불러오기 성공"
@@ -22,12 +31,18 @@ router.get('/', (req, res) => {
 });
 
 // 회원 정보 수정
+// @route GET http://localhost:2055/user
+// @desc user update
+// @access Private
 router.patch('/', (req, res) => {
     res.json({
         msg: "회원 정보 수정 성공"
     });
 });
 //탈퇴
+// @route GET http://localhost:2055/user
+// @desc user delete
+// @access Private
 router.delete('/', (req, res) => {
     res.json({
         msg: "회원 탈퇴"
