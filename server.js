11d62c29254cd5be0d11197aa0f75cb55e1b52d4
bob <ehdrud1332@gmail.com>
2020-03-07 10:13:12 +0900

    modify file

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotEnv = require('dotenv');
+//미들웨어 프로젝트 전체에 사용하겠다.
+dotEnv.config();
 
-const db = "mongodb+srv://cheese:0326@cluster0-eelvp.mongodb.net/test?retryWrites=true&w=majority"
 
-mongoose.connect(db, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 })
@@ -17,6 +19,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const port = 2055;
+const port = process.env.PORT;
 //$: 자바스크립트를 불러옴
 app.listen(port, () => console.log(`server started at ${port}`));
